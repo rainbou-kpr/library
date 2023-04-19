0a6e765e9a95a2cb7ae6f282087368ab3ba5ed07
kowerkoint <kowerkoint@gmail.com>
2023-04-19 18:52:56 +0900

    fix shortest-path test

--- test/yosupo-shortest-path.test.cpp
+++ test/yosupo-shortest-path.test.cpp
@@ -4,13 +4,19 @@
 
 int main() {
     int n, m, s, t; std::cin >> n >> m >> s >> t;
-    Graph<long long> g(n); g.read(m, 0);
+    Graph<long long> g(n); g.read(m, 0, true, true);
     auto [dist, prev] = g.shortest_path(s);
-    std::vector<std::pair<int, int>> route;
-    while(t != s) {
-        route.emplace_back(prev[t].from, prev[t].to);
-        t = prev[t];
+    if(dist[t] == std::numeric_limits<long long>::max()) {
+        std::cout << -1 << '\n';
+        return 0;
     }
+    std::vector<std::pair<int, int>> route;
+    int cur = t;
+    while(cur != s) {
+        route.emplace_back(prev[cur].from, prev[cur].to);
+        cur = prev[cur].from;
+    }
+    std::cout << dist[t] << ' ' << route.size() << '\n';
     for(auto it = route.rbegin(); it != route.rend(); it++) {
         std::cout << it->first << ' ' << it->second << '\n';
     }

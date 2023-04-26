f7522f769d73115cf2123d1eea66bab332f32e7d
shinchankosen <sophiaringoukosen@gmail.com>
2023-04-26 14:37:47 +0900

    first commit

---
+++ test/yosupo-convolution-mod.cpp
@@ -0,0 +1,12 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/convolution_mod"
+
+#include "../cpp/modint.hpp"
+#include "../cpp/convolution.hpp"
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    return 0;
+}
+
+

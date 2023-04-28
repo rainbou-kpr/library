881d9bf6efb65d386c2e925cea654fb946696b76
kowerkoint <kowerkoint@gmail.com>
2023-04-28 21:56:19 +0900

    add test for user-defined operator

---
+++ test/yosupo-point-set-range-composite.1.test.cpp
@@ -0,0 +1,35 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/point_set_range_composite"
+
+#include <iostream>
+
+#include "../cpp/modint.hpp"
+#include "../cpp/segtree.hpp"
+
+int main(void) {
+    int n, q; std::cin >> n >> q;
+    using S = std::pair<modint998244353, modint998244353>;
+    struct Op {
+        S operator()(const S& l, const S& r) {
+            return std::make_pair(r.first * l.first, r.first * l.second + r.second);
+        }
+    };
+    struct E {
+        S operator()() {
+            return std::make_pair(modint998244353(1), modint998244353(0));
+        }
+    };
+    std::vector<S> v(n);
+    for(int i = 0; i < n; i++) std::cin >> v[i].first >> v[i].second;
+    StaticSegTree<S, Op, E> seg(v);
+    while(q--) {
+        int op; std::cin >> op;
+        if(op == 0) {
+            int p; modint998244353 c, d; std::cin >> p >> c >> d;
+            seg.set(p, std::make_pair(c, d));
+        } else {
+            int l, r; modint998244353 x; std::cin >> l >> r >> x;
+            S f = seg.prod(l, r);
+            std::cout << f.first * x + f.second << '\n';
+        }
+    }
+}

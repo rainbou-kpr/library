31ce6a877e9fd2d33460748013b628dd6c17556a
shinchankosen <sophiaringoukosen@gmail.com>
2023-05-03 11:59:55 +0900

    sqrtll doxygen

--- cpp/sqrt.hpp
+++ cpp/sqrt.hpp
@@ -2,12 +2,12 @@
 
 /**
  * @file sqrt.hpp
- * @brief floor(sqrt(n))を求める
+ * @brief floor(sqrt(n))を二分探索で求める
+ * @param n 平方根を求めたい整数 long long 型で 0 <= n < 2^63 を満たす
  */
-
-// 0 <= n <= 9 * 10^18
 long long sqrtll(long long n) { 
-    long long le = 0, ri = 3'000'000'001;
+    // 3037000500 = ceil(sqrt(2^63))
+    long long le = 0, ri = 3037000500; 
     while(ri - le > 1) {
         long long mid = (le + ri) / 2;
         if(mid * mid <= n) le = mid;

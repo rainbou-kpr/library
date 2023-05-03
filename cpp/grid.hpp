4f0f2c62b19f4ed5740b60cd77e55bab340c6dda
shinchankosen <sophiaringoukosen@gmail.com>
2023-05-03 21:03:14 +0900

    grid doxygen

--- cpp/grid.hpp
+++ cpp/grid.hpp
@@ -3,10 +3,25 @@
 #include <vector>
 #include <algorithm>
 
+/**
+ * @brief 幾何学としてのベクトル
+ *
+ * @tparam T 型 stringまたはvector<N> Nはintやlong longなどの整数型
+ */
 template<class T> struct Grid {
     std::vector<T> v;
     int n, m;
+    /**
+     * @brief コンストラクタ
+     * @param v グリッドの元となる vector<string> や vector<vector<int>>などの配列
+     * @return Grid
+     */
     constexpr Grid(std::vector<T> v) noexcept : v(v), n(v.size()) { m = (n ? (int)v[0].size() : 0); }
+
+    /**
+     * @brief 転置
+     * @return Grid
+     */
     constexpr Grid transpose() noexcept {
         if(n == 0) return *this;
         std::vector<T> ret(m);
@@ -16,16 +31,31 @@ template<class T> struct Grid {
         }
         return Grid(ret);
     }
+
+    /**
+     * @brief 左右反転
+     * @return Grid
+     */
     constexpr Grid rev_lr() noexcept {
         std::vector<T> ret = v;
         for(int i = 0; i < n; i ++) reverse(ret[i].begin(), ret[i].end());
         return Grid(ret);
     }
+
+    /**
+     * @brief 上下反転
+     * @return Grid
+     */
     constexpr Grid rev_ud() noexcept {
         std::vector<T> ret = v;
         reverse(ret.begin(), ret.end());
         return Grid(ret);
     }
+
+    /**
+     * @brief 時計周りに90度回転
+     * @return Grid
+     */
     constexpr Grid rotate(int k) noexcept {
         k %= 4;
         if(k == 0) return *this;
@@ -45,11 +75,23 @@ template<class T> struct Grid {
         }
         return Grid(ret);
     }
+
+    /**
+     * @brief 割った余りを[0, mod-1]の範囲で求める
+     * @param x 割られる数
+     * @param mod modをとる数
+     * @return int
+     */
     constexpr int safe_mod(int x, int mod) noexcept {
         x %= mod;
         if (x < 0) x += mod;
         return x;
     }
+
+    /**
+     * @brief (i, j)を((i + dy) mod n, (j + dx) mod m)に移動
+     * @return Grid
+     */
     constexpr Grid shift(int dy, int dx) noexcept {
         std::vector<T> ret = v;
         for(int i = 0; i < n; i ++) {
@@ -59,15 +101,35 @@ template<class T> struct Grid {
         }
         return Grid(ret);
     }
+
+    /**
+     * @brief 左にk回シフト
+     * @return Grid
+     */
     constexpr Grid shift_l(int k) noexcept {
         return this->shift(0, -k);
     }
+
+    /**
+     * @brief 右にk回シフト
+     * @return Grid
+     */
     constexpr Grid shift_r(int k) noexcept {
         return this->shift(0, k);
     }
+
+    /**
+     * @brief 上にk回シフト
+     * @return Grid
+     */
     constexpr Grid shift_u(int k) noexcept {
         return this->shift(-k, 0);
     }
+
+    /**
+     * @brief 下にk回シフト
+     * @return Grid
+     */
     constexpr Grid shift_d(int k) noexcept {
         return this->shift(k, 0);
     }

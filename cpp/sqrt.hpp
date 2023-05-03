3896e8b85f917f44fb1ee4c0d03b487fcad02cf7
shinchankosen <sophiaringoukosen@gmail.com>
2023-05-03 09:49:28 +0900

    sqrt doxygen

--- cpp/sqrt.hpp
+++ cpp/sqrt.hpp
@@ -1,9 +1,11 @@
 #pragma once
 
 /**
- * @brief floor(sqrt(n))
+ * @file sqrt.hpp
+ * @brief floor(sqrt(n))を求める
  */
 
+// 0 <= n <= 9 * 10^18
 long long sqrtll(long long n) { 
     long long le = 0, ri = 3'000'000'001;
     while(ri - le > 1) {

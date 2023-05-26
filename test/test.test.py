d1646986dff482caec98b4dee00aec7d81e8b3fa
shogo314 <shg.kawasome@gmail.com>
2023-05-26 23:53:50 +0900

    6

--- test/test.test.py
+++ test/test.test.py
@@ -1,20 +1,5 @@
-# verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/all/DSL_1_A
-import sys
-input = sys.stdin.buffer.readline
-
-from py.unionfind import UnionFind
+# verification-helper: PROBLEM https://judge.yosupo.jp/problem/aplusb
+from py.add import add
 
 
-def main() -> None:
-    N, Q = map(int, input().split())
-    uft = UnionFind(N)
-    for _ in range(Q):
-        t, u, v = map(int, input().split())
-        if t == 0:
-            uft.merge(u, v)
-        else:
-            print(int(uft.same(u, v)))
-
-
-if __name__ == "__main__":
-    main()
+print(add(*map(int, input().split())))

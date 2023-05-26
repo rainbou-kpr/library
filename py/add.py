ff1e0166eaacbb6800e11e6a99e19f79343980a3
shogo314 <shg.kawasome@gmail.com>
2023-05-27 00:26:30 +0900

    8

--- py/add.py
+++ py/add.py
@@ -1,6 +1,8 @@
-class add:
-    def __init__(self, x, y):
+class sub:
+    def __init__(self, x, y) -> None:
         self.v = x+y
 
+
+class add(sub):
     def __str__(self):
         return str(self.v)

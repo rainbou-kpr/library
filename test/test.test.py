154abda45fe9a9c39ccf1c53fe1e1e30ce99931c
shogo314 <shg.kawasome@gmail.com>
2023-05-26 23:04:52 +0900

    1

---
+++ test/test.test.py
@@ -0,0 +1,17 @@
+# verification-helper: PROBLEM https://judge.yosupo.jp/problem/unionfind
+from py.unionfind import UnionFind
+
+
+def main():
+    N, Q = map(int, input().split())
+    g = UnionFind(N)
+    for i in range(Q):
+        t, u, v = map(int, input().split())
+        if t == 0:
+            g.merge(u, v)
+        else:
+            print(1 if g.same(u, v) else 0)
+
+
+if __name__ == "__main__":
+    main()

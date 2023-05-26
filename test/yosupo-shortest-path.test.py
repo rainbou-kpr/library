b7ea47082d259246d3da5a3e64a53770608194f5
shogo314 <shg.kawasome@gmail.com>
2023-05-26 10:29:37 +0900

    add def main to .py test

--- test/yosupo-shortest-path.test.py
+++ test/yosupo-shortest-path.test.py
@@ -1,17 +1,19 @@
 # verification-helper: PROBLEM https://judge.yosupo.jp/problem/shortest_path
 from py.graph import Graph
 
-n, m, s, t = map(int, input().split())
-g = Graph(n)
-g.read(m, 0, True, True)
-dist, prev = g.shortest_path(s, True)
-if dist[t] == -1:
-    exit(print(-1))
-route = []
-cur = t
-while cur != s:
-    route.append((prev[cur].src, prev[cur].dst))
-    cur = prev[cur].src
-print(dist[t], len(route))
-for src, dst in route[::-1]:
-    print(src, dst)
+
+def main():
+    n, m, s, t = map(int, input().split())
+    g = Graph(n)
+    g.read(m, 0, True, True)
+    dist, prev = g.shortest_path(s, True)
+    if dist[t] == -1:
+        exit(print(-1))
+    route = []
+    cur = t
+    while cur != s:
+        route.append((prev[cur].src, prev[cur].dst))
+        cur = prev[cur].src
+    print(dist[t], len(route))
+    for src, dst in route[::-1]:
+        print(src, dst)
--- test/yosupo-unionfind.test.py
+++ test/yosupo-unionfind.test.py
@@ -1,11 +1,13 @@
 # verification-helper: PROBLEM https://judge.yosupo.jp/problem/unionfind
 from py.unionfind import UnionFind
 
-N, Q = map(int, input().split())
-g = UnionFind(N)
-for i in range(Q):
-    t, u, v = map(int, input().split())
-    if t == 0:
-        g.merge(u, v)
-    else:
-        print(1 if g.same(u, v) else 0)
+
+def main():
+    N, Q = map(int, input().split())
+    g = UnionFind(N)
+    for i in range(Q):
+        t, u, v = map(int, input().split())
+        if t == 0:
+            g.merge(u, v)
+        else:
+            print(1 if g.same(u, v) else 0)

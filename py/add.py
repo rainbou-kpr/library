87111a5d53894727cefbc9325c8a9e8b178eadd4
shogo314 <shg.kawasome@gmail.com>
2023-05-27 01:22:15 +0900

    16

--- py/add.py
+++ py/add.py
@@ -6,12 +6,12 @@ def add(*l):
         t += i
     return t
 
-# from typing import List, TypeVar
-# T = TypeVar('T')
+from typing import List, TypeVar
+T = TypeVar('T')
 
 
-# def add(l: List[T]) -> T:
-#     t = 0
-#     for i in l:
-#         t += i
-#     return t
+def add(l: List[T]) -> T:
+    t = 0
+    for i in l:
+        t += i
+    return t

eb192eff6e91b99f54f2d45cca95d6f18f62e9f7
shogo314 <shg.kawasome@gmail.com>
2023-05-27 05:14:10 +0900

    23

--- py/test.py
+++ py/test.py
@@ -1,12 +1,14 @@
-from collections import defaultdict
-
-
 class Dict:
     def __init__(self) -> None:
-        self.d = defaultdict(int)
+        self.d = {}
 
     def __getitem__(self, x: int) -> int:
-        return self.d[str(x)]
+        if str(x) in self.d:
+            return self.d[str(x)]
+        else:
+            return 0
 
     def __setitem__(self, x: int, y: int) -> None:
         self.d[str(x)] = y
+
+import math

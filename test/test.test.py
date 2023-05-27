3ffcc85fee49180fdf630f8867d20f07f5f1216e
shogo314 <shg.kawasome@gmail.com>
2023-05-27 19:48:04 +0900

    33

--- test/test.test.py
+++ test/test.test.py
@@ -2,16 +2,11 @@
 from py.test import UnionFindTree
 
 
-def main():
-    N, Q = map(int, input().split())
-    g = UnionFindTree(N)
-    for i in range(Q):
-        t, u, v = map(int, input().split())
-        if t == 0:
-            g.merge(u, v)
-        else:
-            print(1 if g.same(u, v) else 0)
-
-
-if __name__ == "__main__":
-    main()
+N, Q = map(int, input().split())
+g = UnionFindTree(N)
+for i in range(Q):
+    t, u, v = map(int, input().split())
+    if t == 0:
+        g.merge(u, v)
+    else:
+        print(1 if g.same(u, v) else 0)

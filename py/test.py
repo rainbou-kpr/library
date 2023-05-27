c57a9e8c4cad3729a8a5fb4a01a20046663e2610
shogo314 <shg.kawasome@gmail.com>
2023-05-27 17:51:36 +0900

    31

--- py/test.py
+++ py/test.py
@@ -1,50 +1,5 @@
-import heapq
+import math
 
 
-class DoubleEndedPriorityQueue:
-    def __init__(self) -> None:
-        self.l = []
-        self.ld = {}
-        self.r = []
-        self.rd = {}
-
-    def eject(self, x: int) -> None:
-        if x in self.ld:
-            self.ld[x] += 1
-        else:
-            self.ld[x] = 1
-        if x in self.rd:
-            self.rd[x] += 1
-        else:
-            self.rd[x] = 1
-        while bool(self.l) and self.l[0] in self.ld:
-            self.ld[self.l[0]] -= 1
-            if self.ld[self.l[0]] == 0:
-                self.ld.pop(self.l[0])
-            heapq.heappop(self.l)
-        while bool(self.r) and -self.r[0] in self.rd:
-            self.rd[-self.r[0]] -= 1
-            if self.rd[-self.r[0]] == 0:
-                self.rd.pop(-self.r[0])
-            heapq.heappop(self.r)
-
-    def __bool__(self) -> bool:
-        return bool(self.l)
-
-    def pop_min(self) -> int:
-        if not self:
-            raise IndexError("pop from empty")
-        m = self.l[0]
-        self.eject(m)
-        return m
-
-    def pop_max(self) -> int:
-        if not self:
-            raise IndexError("pop from empty")
-        m = -self.r[0]
-        self.eject(m)
-        return m
-
-    def insert(self, x: int) -> None:
-        heapq.heappush(self.l, x)
-        heapq.heappush(self.r, -x)
+def lcm(x, y):
+    return x*y//math.gcd(x, y)
--- test/test.test.py
+++ test/test.test.py
@@ -1,17 +1,4 @@
-# verification-helper: PROBLEM https://judge.yosupo.jp/problem/double_ended_priority_queue
-from py.test import DoubleEndedPriorityQueue
+# verification-helper: PROBLEM https://judge.yosupo.jp/problem/aplusb
+from py.test import lcm
 
-
-pq = DoubleEndedPriorityQueue()
-N, Q = map(int, input().split())
-S = list(map(int, input().split()))
-for i in S:
-    pq.insert(i)
-for _ in range(Q):
-    q = tuple(map(int, input().split()))
-    if q[0] == 0:
-        pq.insert(q[1])
-    elif q[0] == 1:
-        print(pq.pop_min())
-    else:
-        print(pq.pop_max())
+print(sum(list(map(int, input().split())))*lcm(4, 6)//12)

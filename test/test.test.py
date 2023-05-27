cef7223f861f4971995f59bf20c10ace797bcc89
shogo314 <shg.kawasome@gmail.com>
2023-05-27 23:09:49 +0900

    34

--- test/test.test.py
+++ test/test.test.py
@@ -1,9 +1,11 @@
 # verification-helper: PROBLEM https://judge.yosupo.jp/problem/unionfind
+import heapq
+
 from py.test import UnionFindTree
 
 
 N, Q = map(int, input().split())
-g = UnionFindTree(N)
+g = UnionFindTree(N, heapq.heappop, heapq.heappush)
 for i in range(Q):
     t, u, v = map(int, input().split())
     if t == 0:

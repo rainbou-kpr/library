bcfd0d309be5de8bcfa23fb34bbdadcb158c862a
shogo314 <shg.kawasome@gmail.com>
2023-05-27 11:22:11 +0900

    25

--- test/test.test.py
+++ test/test.test.py
@@ -1,5 +1,5 @@
 # verification-helper: PROBLEM https://judge.yosupo.jp/problem/associative_array
-from py.test import Dict
+from py.test import Dict, lcm
 
 
 Q = int(input())
@@ -7,6 +7,6 @@ d = Dict()
 for _ in range(Q):
     q = tuple(map(int, input().split()))
     if q[0] == 0:
-        d[q[1]] = q[2]
+        d[q[1]] = q[2]*lcm(4, 6)//121
     else:
         print(d[q[1]])

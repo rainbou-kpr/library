fb4e202fc0526e5839a0dee66e4a039014db3389
shogo314 <shg.kawasome@gmail.com>
2023-05-27 23:26:52 +0900

    35

--- py/test.py
+++ py/test.py
@@ -1,49 +1,6 @@
-class DoubleEndedPriorityQueue:
-    def __init__(self,pop,push) -> None:
-        self.pop=pop
-        self.push=push
-        self.l = []
-        self.ld = {}
-        self.r = []
-        self.rd = {}
+import sys
 
-    def eject(self, x: int) -> None:
-        if x in self.ld:
-            self.ld[x] += 1
-        else:
-            self.ld[x] = 1
-        if x in self.rd:
-            self.rd[x] += 1
-        else:
-            self.rd[x] = 1
-        while bool(self.l) and self.l[0] in self.ld:
-            self.ld[self.l[0]] -= 1
-            if self.ld[self.l[0]] == 0:
-                self.ld.pop(self.l[0])
-            self.pop(self.l)
-        while bool(self.r) and -self.r[0] in self.rd:
-            self.rd[-self.r[0]] -= 1
-            if self.rd[-self.r[0]] == 0:
-                self.rd.pop(-self.r[0])
-            self.pop(self.r)
 
-    def __bool__(self) -> bool:
-        return bool(self.l)
-
-    def pop_min(self) -> int:
-        if not self:
-            raise IndexError("pop from empty")
-        m = self.l[0]
-        self.eject(m)
-        return m
-
-    def pop_max(self) -> int:
-        if not self:
-            raise IndexError("pop from empty")
-        m = -self.r[0]
-        self.eject(m)
-        return m
-
-    def insert(self, x: int) -> None:
-        self.push(self.l, x)
-        self.push(self.r, -x)
+def Main():
+    input = sys.stdin.buffer.readline
+    print(sum(list(map(int, input().split()))))
--- test/test.test.py
+++ test/test.test.py
@@ -1,19 +1,6 @@
-# verification-helper: PROBLEM https://judge.yosupo.jp/problem/double_ended_priority_queue
-import heapq
+# verification-helper: PROBLEM https://judge.yosupo.jp/problem/aplusb
 
-from py.test import DoubleEndedPriorityQueue
+from py.test import Main
 
 
-pq = DoubleEndedPriorityQueue(heapq.heappop, heapq.heappush)
-N, Q = map(int, input().split())
-S = list(map(int, input().split()))
-for i in S:
-    pq.insert(i)
-for _ in range(Q):
-    q = tuple(map(int, input().split()))
-    if q[0] == 0:
-        pq.insert(q[1])
-    elif q[0] == 1:
-        print(pq.pop_min())
-    else:
-        print(pq.pop_max())
+Main()

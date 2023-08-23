cdee4746dc9f6587ce4f268b96c06c954d572bd5
shogo314 <shg.kawasome@gmail.com>
2023-08-23 17:41:47 +0900

    change back to end ,and others

--- py/linkedlist.py
+++ py/linkedlist.py
@@ -18,21 +18,21 @@ class LinkedList:
                 return self.prev.__isub__(k-1)
 
     def __init__(self, l=None) -> None:
-        self.front = None
-        self.back = None
+        self.begin = self.LinkedListIterator(None)
+        self.end = self.begin
         if l is not None:
             for v in l:
                 self.append(v)
 
     def __bool__(self) -> bool:
-        return self.front is not None
+        return self.begin is not self.end
 
     def __iter__(self):
-        self._a = self.front
+        self._a = self.begin
         return self
 
     def __next__(self):
-        if self._a is None:
+        if self._a is self.end:
             raise StopIteration()
         else:
             rt = self._a.value
@@ -41,60 +41,59 @@ class LinkedList:
 
     def append(self, v) -> None:
         itr = self.LinkedListIterator(v)
-        itr.prev = self.back
-        if self.front is None:
-            self.front = itr
+        if self.begin is self.end:
+            self.begin = itr
         else:
-            self.back.next = itr
-        self.back = itr
+            itr.prev = self.end.prev
+            self.end.prev.next = itr
+        itr.next = self.end
+        self.end.prev = itr
 
     def appendleft(self, v) -> None:
         itr = self.LinkedListIterator(v)
-        itr.next = self.front
-        if self.back is None:
-            self.back = itr
-        else:
-            self.front.prev = itr
-        self.front = itr
+        itr.next = self.begin
+        self.begin.prev = itr
+        self.begin = itr
 
     def pop(self):
-        itr = self.back
-        if self.front is itr:
-            self.front = self.back = None
+        assert(self)
+        itr = self.end.prev
+        if self.begin is itr:
+            self.end.prev = None
+            self.begin = self.end
         else:
-            self.back = itr.prev
-            self.back.next = None
+            itr.prev.next = self.end
+            self.end.prev = itr
         return itr.value
 
     def popleft(self):
-        itr = self.front
-        if self.back is itr:
-            self.front = self.back = None
+        assert(self)
+        itr = self.begin
+        if itr.next is self.end:
+            self.end.prev = None
+            self.begin = self.end
         else:
-            self.front = itr.next
-            self.front.prev = None
+            itr.next.prev = None
+            self.begin = itr.next
         return itr.value
 
     def insert(self, i, v) -> None:
-        if i is None:
+        if i is self.begin:
             self.appendleft(v)
-        if i is self.back:
+        if i is self.end:
             self.append(v)
         else:
-            j = i.next
+            j = i.prev
             itr = self.LinkedListIterator(v)
-            itr.prev = i
-            itr.next = j
-            j.prev = itr
-            i.next = itr
+            itr.prev = j
+            itr.next = i
+            j.next = itr
+            i.prev = itr
 
     def erase(self, i):
-        assert(self)
-        assert(i is not None)
-        if i is self.front:
-            return self.popleft()
-        elif i is self.back:
-            return self.pop()
+        assert(i is not self.end)
+        if i is self.begin:
+            self.popleft()
         else:
             j = i.next
             i = i.prev
@@ -102,8 +101,117 @@ class LinkedList:
             j.prev = i
 
     def index(self, v):
-        itr = self.front
-        while itr is not None:
+        for itr in self:
+            if itr.value == v:
+                return itr
+        return None
+
+
+class LinkedList:
+    class LinkedListIterator:
+        def __init__(self, v) -> None:
+            self.value = v
+            self.prev = None
+            self.next = None
+
+        def __iadd__(self, k: int):
+            if k == 1:
+                return self.next
+            else:
+                return self.next.__iadd__(k-1)
+
+        def __isub__(self, k: int):
+            if k == 1:
+                return self.prev
+            else:
+                return self.prev.__isub__(k-1)
+
+    def __init__(self, l=None) -> None:
+        self.begin = self.LinkedListIterator(None)
+        self.end = self.begin
+        if l is not None:
+            for v in l:
+                self.append(v)
+
+    def __bool__(self) -> bool:
+        return self.begin is not self.end
+
+    def __iter__(self):
+        self._a = self.begin
+        return self
+
+    def __next__(self):
+        if self._a is self.end:
+            raise StopIteration()
+        else:
+            rt = self._a.value
+            self._a += 1
+            return rt
+
+    def append(self, v) -> None:
+        itr = self.LinkedListIterator(v)
+        if self.begin is self.end:
+            self.begin = itr
+        else:
+            itr.prev = self.end.prev
+            self.end.prev.next = itr
+        itr.next = self.end
+        self.end.prev = itr
+
+    def appendleft(self, v) -> None:
+        itr = self.LinkedListIterator(v)
+        itr.next = self.begin
+        self.begin.prev = itr
+        self.begin = itr
+
+    def pop(self):
+        assert(self)
+        itr = self.end.prev
+        if self.begin is itr:
+            self.end.prev = None
+            self.begin = self.end
+        else:
+            itr.prev.next = self.end
+            self.end.prev = itr.prev
+        return itr.value
+
+    def popleft(self):
+        assert(self)
+        itr = self.begin
+        if itr.next is self.end:
+            self.end.prev = None
+            self.begin = self.end
+        else:
+            itr.next.prev = None
+            self.begin = itr.next
+        return itr.value
+
+    def insert(self, i, v) -> None:
+        if i is self.begin:
+            self.appendleft(v)
+        if i is self.end:
+            self.append(v)
+        else:
+            j = i.prev
+            itr = self.LinkedListIterator(v)
+            itr.prev = j
+            itr.next = i
+            j.next = itr
+            i.prev = itr
+
+    def erase(self, i):
+        assert(i is not self.end)
+        if i is self.begin:
+            self.popleft()
+        else:
+            j = i.next
+            i = i.prev
+            i.next = j
+            j.prev = i
+
+    def index(self, v):
+        itr = self.begin
+        while itr is not self.end:
             if itr.value == v:
                 return itr
             else:

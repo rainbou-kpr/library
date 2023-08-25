57a6ce8eb8f7520742bda6c98451a2a9da13fd0f
shogo314 <shg.kawasome@gmail.com>
2023-08-25 17:40:41 +0900

    add extend and more

--- py/linkedlist.py
+++ py/linkedlist.py
@@ -1,8 +1,4 @@
-from typing import Any, List, Optional
-
-
-class LinkedListElement:
-    pass
+from typing import Any, List, Optional, Iterable
 
 
 class LinkedListElement:
@@ -10,25 +6,42 @@ class LinkedListElement:
     連結リストの要素
     """
 
-    def __init__(self, value: Any, prev: Optional[LinkedListElement] = None, next: Optional[LinkedListElement] = None) -> None:
+    def __init__(self, value: Any, prev: Optional['LinkedListElement'] = None, next: Optional['LinkedListElement'] = None) -> None:
         """
         コンストラクタ
+        :param Any value: 値
+        :param LinkedListElement | None prev: 前の要素
+        :param LinkedListElement | None next: 次の要素
         """
         self.value = value
         self.prev = prev
         self.next = next
 
-    def __iadd__(self, k: int) -> Optional[LinkedListElement]:
+    def __iadd__(self, k: int) -> Optional['LinkedListElement']:
         a = self
         for i in range(k):
             a = a.next
-        return self.next
+            if a is None:
+                return None
+        return a
 
-    def __isub__(self, k: int) -> Optional[LinkedListElement]:
+    def __add__(self, k: int) -> Optional['LinkedListElement']:
+        a = self
+        a += k
+        return a
+
+    def __isub__(self, k: int) -> Optional['LinkedListElement']:
         a = self
         for i in range(k):
             a = a.prev
+            if a is None:
+                return None
         return self.prev
+
+    def __sub__(self, k: int) -> Optional['LinkedListElement']:
+        a = self
+        a -= k
+        return a
 
 
 class LinkedListIterator:
@@ -50,14 +63,17 @@ class LinkedListIterator:
 
 
 class LinkedList:
-    def __init__(self, l=None) -> None:
+    def __init__(self, l: Optional[Iterable] = None) -> None:
+        """
+        コンストラクタ
+        :param iterable l: 初期値
+        """
         self.begin = LinkedListElement(None)
         self.end = self.begin
         self.size = 0
         if l is not None:
             for v in l:
                 self.append(v)
-                self.size += 1
 
     def __bool__(self) -> bool:
         return self.begin is not self.end
@@ -68,6 +84,29 @@ class LinkedList:
     def __len__(self) -> int:
         return self.size
 
+    def __getitem__(self, k: int) -> Any:
+        """
+        前からk番目(0-based)の値を返す。O(min(k,len-k))
+        :param int k: index
+        """
+        assert(0 <= k < self.size)
+        if k <= self.size//2:
+            return (self.begin+k).value
+        else:
+            return (self.end-(self.size-k)).value
+
+    def __setitem__(self, k: int, v: Any) -> None:
+        """
+        前からk番目(0-based)の値に代入する。O(min(k,len-k))
+        :param int k: index
+        :param Any v: 値
+        """
+        assert(0 <= k < self.size)
+        if k <= self.size//2:
+            (self.begin+k).value = v
+        else:
+            (self.end-(self.size-k)).value = v
+
     def values(self) -> List[Any]:
         """
         各要素の値を順に並べたリストを返す
@@ -77,6 +116,7 @@ class LinkedList:
     def append(self, v: Any) -> None:
         """
         末尾に追加する
+        :param Any v: 値
         """
         self.size += 1
         itr = LinkedListElement(v, self.end.prev, self.end)
@@ -89,11 +129,40 @@ class LinkedList:
     def appendleft(self, v) -> None:
         """
         先頭に追加する
+        :param Any v: 値
         """
         self.size += 1
         itr = LinkedListElement(v, None, self.begin)
         self.begin.prev = itr
         self.begin = itr
+
+    def extend(self, l: "LinkedList") -> None:
+        """
+        lを末尾に追加する。\\
+        LinkedListならO(1)、それ以外はO(len(l))。
+        """
+        self.size += l.size
+        if self.begin is self.end:
+            self.begin = l.begin
+            self.end = l.end
+        else:
+            l.begin.prev = self.end.prev
+            self.end.prev.next = l.begin
+            self.end = l.end
+
+    def extendleft(self, l: "LinkedList") -> None:
+        """
+        lを先頭に追加する。\\
+        LinkedListならO(1)、それ以外はO(len(l))。
+        """
+        self.size += l.size
+        if self.begin is self.end:
+            self.begin = l.begin
+            self.end = l.end
+        else:
+            self.begin.prev = l.end.prev
+            l.end.prev.next = self.begin
+            self.begin = l.begin
 
     def pop(self) -> Any:
         """
@@ -140,7 +209,7 @@ class LinkedList:
             j.next = itr
             i.prev = itr
 
-    def erase(self, i: LinkedListElement):
+    def erase(self, i: LinkedListElement) -> None:
         """
         指定したものを削除する
         """
@@ -165,7 +234,7 @@ class LinkedList:
 
     def index(self, v: Any) -> Optional[int]:
         """
-        指定した値が何番目か返す(0-based)
+        指定した値が何番目かを返す(0-based)
         """
         k = 0
         for itr in self:

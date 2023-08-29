6973b8e44fb6ec7e5c70ce8aae3c0984526a2662
KowerKoint <kowerkoint@gmail.com>
2023-08-29 23:08:45 +0900

    remove base type constructor using declaration

--- cpp/modint.hpp
+++ cpp/modint.hpp
@@ -331,17 +331,15 @@ public:
         return Mod;
     }
 
-    using typename base_type::modint_base;
-
     /// @brief 0 で初期化します。
     constexpr static_modint() noexcept
-        : modint_base{} {}
+        : base_type{} {}
 
     /// @brief @c value の剰余で初期化します。
     /// @param value 初期化に使う値
     template <class SignedIntegral, std::enable_if_t<std::is_integral_v<SignedIntegral>>* = nullptr>
     constexpr static_modint(SignedIntegral value) noexcept
-        : modint_base{value} {}
+        : base_type{value} {}
 
     /// @brief 自身の逆数を返します。
     /// @remark 時間計算量： @f$O(\log value)@f$
@@ -382,17 +380,15 @@ public:
         modulus = m;
     }
 
-    using typename base_type::modint_base;
-
     /// @brief 0 で初期化します。
     constexpr dynamic_modint() noexcept
-        : modint_base{} {}
+        : base_type{} {}
 
     /// @brief @c value の剰余で初期化します。
     /// @param value 初期化に使う値
     template <class SignedIntegral, std::enable_if_t<std::is_integral_v<SignedIntegral>>* = nullptr>
     constexpr dynamic_modint(SignedIntegral value) noexcept
-        : modint_base{value} {}
+        : base_type{value} {}
 
 private:
     inline static value_type modulus = 998244353;

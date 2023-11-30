dd92a30844b2d1f7abec98023c280e1fb6a2ba5f
shogo314 <shg.kawasome@gmail.com>
2023-11-30 23:29:28 +0900

    mv topo

--- cpp/graph_util.hpp
+++ cpp/graph_util.hpp
@@ -92,3 +92,28 @@ template <typename Cost = int>
 bool is_tree(const Graph<Cost>& graph) {
     return graph.m == graph.n - 1 && is_connected(graph);
 }
+
+/**
+ * @brief 有向グラフをトポロジカルソートする
+ * @param G トポロジカルソートするグラフ
+ * @return ソートされたノード番号のvector DAGでなければ長さがG.n未満になる
+ */
+template<typename Cost>
+std::vector<int> topological_sort(const Graph<Cost> &G) {
+    std::vector<int> indeg(G.n), sorted;
+    std::queue<int> q;
+    for (int i = 0; i < G.n; i++) {
+        for (int dst : G[i]) indeg[dst]++;
+    }
+    for (int i = 0; i < G.n; i++) {
+        if (!indeg[i]) q.push(i);
+    }
+    while (!q.empty()) {
+        int cur = q.front(); q.pop();
+        for (int dst : G[cur]) {
+            if (!--indeg[dst]) q.push(dst);
+        }
+        sorted.push_back(cur);
+    }
+    return sorted;
+}
--- test/atcoder-edpc-g.test.cpp
+++ test/atcoder-edpc-g.test.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <algorithm>
 
-#include "../cpp/graph.hpp"
+#include "../cpp/graph_util.hpp"
 
 int main(void){
 
@@ -12,8 +12,8 @@ int main(void){
     Graph G(N);
     G.read(M, -1, false, true);
 
-    std::vector<int> dist(N);
-    for(int x : G.topological_sort()){
+    std::vector<int> dist(N), ord(topological_sort(G));
+    for(int x : ord){
         for(int y : G[x]) dist[y] = std::max(dist[y], dist[x] + 1);
     }
     std::cout << *std::max_element(dist.begin(), dist.end()) << std::endl;
